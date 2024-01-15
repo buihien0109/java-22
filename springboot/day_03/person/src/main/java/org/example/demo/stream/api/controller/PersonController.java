4c47cdfe268f9a533039f9fd9a5a1e4c4d66cbbf
buihien0109 <buihien01091997@gmail.com>
2024-01-15 20:25:40 +0700

    update

--- springboot/day_03/person/src/main/java/org/example/demo/stream/api/controller/PersonController.java
+++ springboot/day_03/person/src/main/java/org/example/demo/stream/api/controller/PersonController.java
@@ -1,2 +1,31 @@
-package org.example.demo.stream.api.controller;public class PersonController {
+package org.example.demo.stream.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.demo.stream.api.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class PersonController {
+    @Autowired
+    private PersonDAO personDAO;
+
+    @GetMapping("/")
+    public String getHome() {
+        return "index";
+    }
+
+    @GetMapping("/getAll")
+    public String getAll(Model model) {
+        model.addAttribute("people", personDAO.getAll());
+        return "getAll";
+    }
+
+    @GetMapping("/sortPeopleByFullName")
+    public String sortPeopleByFullName() {
+        return "sortPeopleByFullName";
+    }
 }
