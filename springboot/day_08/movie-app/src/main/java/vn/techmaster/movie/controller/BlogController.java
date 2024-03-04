54a6e22bcecf5e3e9c05ceb69b26301e9788511f
buihien0109 <buihien01091997@gmail.com>
2024-03-04 20:59:30 +0700

    update

--- springboot/day_08/movie-app/src/main/java/vn/techmaster/movie/controller/BlogController.java
+++ springboot/day_08/movie-app/src/main/java/vn/techmaster/movie/controller/BlogController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vn.techmaster.movie.service.BlogService;
+import vn.techmaster.movie.service.ImageService;
 
 @Controller
 @RequestMapping("admin/blogs")
 @RequiredArgsConstructor
 public class BlogController {
     private final BlogService blogService;
+    private final ImageService imageService;
 
     @GetMapping
     public String viewHomePage(Model model) {
@@ -32,7 +34,8 @@ public class BlogController {
     }
 
     @GetMapping("/{id}/detail")
-    public String viewDetailPage(@PathVariable Integer id) {
+    public String viewDetailPage(@PathVariable Integer id, Model model) {
+        model.addAttribute("images", imageService.getAllImagesByCurrentUser());
         return "admin/blog/detail";
     }
 }
--- springboot/day_08/movie-app/src/main/resources/static/admin-assets/dist/js/common.js
+++ springboot/day_08/movie-app/src/main/resources/static/admin-assets/dist/js/common.js
@@ -1,3 +1,21 @@
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
 // Active menu
 const activeMenu = () => {
     const menuParentEls = document.querySelectorAll("#main-menu > li > a");
